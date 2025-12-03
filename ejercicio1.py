c45928c371ca090d5aa88f31dcf82e192943e1cc
luismendoza107 <mendoza25luism@gmail.com>
2025-12-03 11:03:47 -0500

    Ejercicio1_clonado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,20 +1,13 @@
-'''
-Un técnico de soporte necesita ingresar a una consola protegida por contraseña. Escribe un programa que:
+def verifique_su_contraseña():
+    system_password = "admin123" 
+    user_password = input("Ingresa la contraseña, por favor: ")
 
-1.Guarde la contraseña correcta en una variable ("admin123").
-2. Pida al usuario ingresar una contraseña.
-3. Verifique si la contraseña ingresada es correcta o no
+#Paso 4:
+    if system_password == user_password:
+        print("Contraseña correcta, bienvenido :D")
+    else:
+        print("Contraseña incorrecta")
 
-'''
-#Paso 1
-system_password = "admin123"
-
-#paso 2
-user_password = input("Ingresa la contraseña, por favor: ")
-
-#Paso 3:
-if system_password == user_password:
-    print("Contraseña correcta, bienvenido :D")
-else:
-    print("Contraseña incorrecta")
+verifique_su_contraseña()
+   
 
